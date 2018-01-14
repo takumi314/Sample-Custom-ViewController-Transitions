108b6ecd0116fa599cb996375785f19c7c1ada7d
takumi314 <isospin_symmetry@me.com>
2018-01-15 01:15:06 +0900

    add new class for creating Animator

---
+++ Sample-Custom-ViewController-Transitions/New Group/FlipPresentAnimationController.swift
@@ -0,0 +1,13 @@
+//
+//  FlipPresentAnimationController.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/15.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class FlipPresentAnimationController: NSObject {
+
+}
