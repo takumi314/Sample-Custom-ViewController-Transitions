b7f0a88096845538830b2549be46256f71256fde
takumi314 <isospin_symmetry@me.com>
2018-01-15 23:23:58 +0900

    make methods for rotating around each axis

--- Sample-Custom-ViewController-Transitions/Helpers/AnimationHelper.swift
+++ Sample-Custom-ViewController-Transitions/Helpers/AnimationHelper.swift
@@ -10,5 +10,20 @@ import UIKit
 
 class AnimationHelper {
 
+    static func rotate(_ angle: Double, x: Double, y: Double, z: Double) -> CATransform3D {
+        return CATransform3DMakeRotation(CGFloat(angle), CGFloat(x), CGFloat(y), CGFloat(z))
+    }
+
+    static func xRotate(_ angle: Double) -> CATransform3D {
+        return rotate(angle, x: 1, y: 0, z: 0)
+    }
+
+    static func yRotate(_ angle: Double) -> CATransform3D {
+        return rotate(angle, x: 0, y: 1, z: 0)
+    }
+
+    static func zRotate(_ angle: Double) -> CATransform3D {
+        return rotate(angle, x: 0, y: 0, z: 1)
+    }
 
 }
