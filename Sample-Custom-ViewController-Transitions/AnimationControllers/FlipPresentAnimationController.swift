e8eb83b85025cab4f24c4635a40e389914a81a9a
takumi314 <isospin_symmetry@me.com>
2018-01-15 20:34:27 +0900

    update/add class file in the group

--- Sample-Custom-ViewController-Transitions/AnimationControllers/FlipPresentAnimationController.swift
+++ Sample-Custom-ViewController-Transitions/AnimationControllers/FlipPresentAnimationController.swift
@@ -55,16 +55,37 @@ extension FlipPresentAnimationController: UIViewControllerAnimatedTransitioning 
         containerView.addSubview(snapshot)
         toVC.view.isHidden = true
 
+        //
+//        AnimationHelper.perspectiveTransform(for: containerView)
+//        snapshot.layer.transform = AnimationHelper.yRotation(.pi / 2)
+
         // アニメーション時間を取得する
         let duration = transitionDuration(using: transitionContext)
 
 
+        // 1
         UIView.animateKeyframes(
             withDuration: duration,
             delay: 0,
             options: .calculationModeCubic,
             animations: {
+                // 2
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3) {
+//                    fromVC.view.layer.transform = AnimationHelper.yRotation(-.pi / 2)
+                }
+
+                // 3
+                UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3) {
+//                    snapshot.layer.transform = AnimationHelper.yRotation(0.0)
+                }
+
+                // 4
+                UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3) {
+                    snapshot.frame = finalFrame
+                    snapshot.layer.cornerRadius = 0
+                }
         },
+            // 5
             completion: { _ in
                 toVC.view.isHidden = false
                 snapshot.removeFromSuperview()
---
+++ Sample-Custom-ViewController-Transitions/Helpers/AnimationHelper.swift
@@ -0,0 +1,9 @@
+//
+//  AnimationHelper.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/15.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
