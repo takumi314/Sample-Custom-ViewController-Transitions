9f719185957ed733cdee7fef7f3b3361afd733bc
takumi314 <isospin_symmetry@me.com>
2018-01-17 22:58:18 +0900

    add new file

---
+++ Sample-Custom-ViewController-Transitions/Commons/CommonProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  CommonProtocol.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/17.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
