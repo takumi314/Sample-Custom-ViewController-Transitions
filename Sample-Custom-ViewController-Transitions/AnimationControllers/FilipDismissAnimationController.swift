2a377b87e6cf314f6c2aace171efbe3895ac5bbc
takumi314 <isospin_symmetry@me.com>
2018-01-17 23:32:05 +0900

    add new class for creating animator

---
+++ Sample-Custom-ViewController-Transitions/AnimationControllers/FilipDismissAnimationController.swift
@@ -0,0 +1,13 @@
+//
+//  FilipDismissAnimationController.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/17.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class FilipDismissAnimated: NSObject {
+
+}
