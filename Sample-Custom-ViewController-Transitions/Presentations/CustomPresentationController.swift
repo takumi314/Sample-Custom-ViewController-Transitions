c79c987358bf3e85f2adcbf4faf08e33ceb57d5b
takumi314 <isospin_symmetry@me.com>
2018-01-22 19:37:31 +0900

    add a presentation VIewController slideing upwards to the presented ViewController

---
+++ Sample-Custom-ViewController-Transitions/Presentations/CustomPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomPresentationController.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/22.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class CustomPresentationController: UIPresentationController {
+
+}
