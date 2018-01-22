11c536a8649ab90f2bc3b6979d00efc7632a8960
takumi314 <isospin_symmetry@me.com>
2018-01-22 19:29:20 +0900

    add a file for next ViewController

---
+++ Sample-Custom-ViewController-Transitions/Presentations/PresentationSecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PresentationSecondViewController.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/22.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class PresentationSecondViewController: UIViewController {
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
