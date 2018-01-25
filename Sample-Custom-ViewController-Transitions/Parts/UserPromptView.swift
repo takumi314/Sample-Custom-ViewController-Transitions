6737dd603bd24cd63aeeb00eac8d45ede54f7ea6
takumi314 <isospin_symmetry@me.com>
2018-01-26 02:45:45 +0900

    add swift file

---
+++ Sample-Custom-ViewController-Transitions/Parts/UserPromptView.swift
@@ -0,0 +1,21 @@
+//
+//  UserPromptView.swift
+//  Sample-Custom-ViewController-Transitions
+//
+//  Created by NishiokaKohei on 2018/01/26.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class UserPromptView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
